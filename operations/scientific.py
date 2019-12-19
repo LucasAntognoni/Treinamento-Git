cbee3ce03702c5f5131e9828c8c97616edb37975
dspavieri <dspavieri@gmail.com>
2019-12-19 17:41:00 -0300

    function factorial

--- operations/scientific.py
+++ operations/scientific.py
@@ -1,5 +1,15 @@
 def factorial(n):
-    pass
+    """
+    author:junior
+    n: integer
+    """
+    if type(n) == int:
+        if n > 1:
+            return n*factorial(n-1)
+        else:
+            return 1
+    else:
+        print('not integer')
 
 
 def exponential(n):
